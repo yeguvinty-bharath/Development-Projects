80bd853cce48cd2bb119f6bbaac098a51f3e7264
Yeguvinty Bharath <yeguvintybharath.9247@gmail.com>
2023-05-28 21:19:10 +0530

    Code modifications (#2)

--- DadJokes/script.js
+++ DadJokes/script.js
@@ -1,10 +1,3 @@
-const options = {
-    method: 'GET',
-    headers: {'X-RapidAPI-Key': 'd8818db931msh8f36f90618e900ap183bddjsn434e3a6a48ee'}
-  };
-  
-  
-
   fetch('https://icanhazdadjoke.com/slack')
     .then(response => response.json())
     .then(response => {
